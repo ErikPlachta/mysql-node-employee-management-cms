0155bf2b0a65a74a5571e8141c5b1c3e6fdc7a7f
Erik Plachta <eplachta@gmail.com>
2022-01-23 16:25:09 -0500

    Add schema.sql concept, still needing to test. Add server.js tempalte content, still need to update.

---
+++ db/sceham.sql
@@ -0,0 +1,38 @@
+--------------------------------------------------------------------------------
+-- Defining Database framework
+
+-- Remove if exists
+DROP DATABASE IF EXISTS employee_db;
+-- Make a new database
+CREATE DATABASE employee_db;
+
+-- Tell MySQL what to use
+USE employee_db;
+
+--------------------------------------------------------------------------------
+
+-- Adding a table to database
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL
+);
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT
+);
+
+-- print to console
+DESCRIBE department;
+DESCRIBE role;
+DESCRIBE employee;
